7ad28f347359a2006258a9dfa734422478ccf16f
grisha <Vtorovstugreg@gmail.com>
2022-12-16 18:08:04 +0300

    start

---
+++ GameButton_class.py
@@ -0,0 +1,10 @@
+import pygame
+from GameObject_class import GameObject
+
+
+class GameButton(GameObject):
+    def __init__(self, group, image_adr, size, cords, func, *args, **kwargs):
+        super().__init__(group, image_adr, size, cords, [0, 0])
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
