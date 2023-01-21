462c14f4a7345362d661e261de4db13a5718efdb
grisha <Vtorovstugreg@gmail.com>
2023-01-21 19:01:55 +0300

    debug animations and jump

--- player_class.py
+++ player_class.py
@@ -1,5 +1,4 @@
 import pygame
-
 from GameObject_class import GameObject
 
 
@@ -9,37 +8,34 @@ class Player(GameObject):
         super().__init__(group, animations, size, cords, speed_vector)
         self.bool_jump = False
         self.pomenal = False
-
         self.high_of_jump = 200
 
     def jump(self, tick):
         if not self.pomenal and self.rect.bottomleft[1] <= self.high_of_jump:
             self.pomenal = True
-            self.speed_vector = -90
-        elif self.pomenal and self.cords[1] >= self.rect.topleft:
-            self.rect.move_ip(*self.cords)
+            self.init_vector([90, 200])
+        elif self.pomenal and self.cords[1] <= self.rect.topleft[-1]:
             self.bool_jump = False
+            self.pomenal = False
+            self.inint_vector([0, 0])
 
-    def update(self, time, rotation=0):
-        super().update(time, rotation)
-        for event in pygame.event.get():
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                self.bool_jump = True
-                print('hi')
+    def update(self, time):
+        super().update(time)
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            self.bool_jump = True
         if self.bool_jump:
-            self.speed_vector = -90
-            self.speed_y = 100
+            if not self.pomenal:
+                self.init_vector([-90, 200])
             self.jump(time)
 
 
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((600, 500))
+    screen = pygame.display.set_mode((600, 800))
     clock = pygame.time.Clock()
     running = True
     group = pygame.sprite.Group()
-    sprite = Player(group, ['Player', 'deadpool.png'], [300, 350], [50, 200], [0, 0])
+    sprite = Player(group, {'statick': ['Player', 'deadpool.png']}, [300, 350], [50, 800 - 350], [0, 0])
     while running:
         screen.fill((255, 255, 255))
         tick = clock.tick(60) / 1000
