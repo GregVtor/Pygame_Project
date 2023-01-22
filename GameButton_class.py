96a64d8f887020c45737a565e9d014fb9816eb1c
grisha <Vtorovstugreg@gmail.com>
2023-01-22 21:50:08 +0300

    debug

---
+++ GameButton_class.py
@@ -0,0 +1,42 @@
+import pygame
+from GameObject_class import GameObject
+
+
+class GameButton(GameObject):
+    def __init__(self, group, animations, size, cords, func, *args, **kwargs):
+        super().__init__(group, animations, size, cords, [0, 0])
+        self.func = func
+        self.args = args
+        self.kwargs = kwargs
+
+    def clicked(self):
+        self.func(self.args, self.kwargs)
+
+    def update(self, time):
+        super().update(time)
+        if pygame.mouse.get_pressed()[0] and self.rect.collidepoint(pygame.mouse.get_pos()):
+            self.clicked()
+
+
+def l(*args, **kwargs):
+    print('lol')
+
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((600, 500))
+    clock = pygame.time.Clock()
+    running = True
+    group = pygame.sprite.Group()
+    sprite = GameButton(group, {'start': [['pygame-8-1.png'], 2, 8]},
+                        [50, 50], [50, 200], l)
+    while running:
+        screen.fill((255, 255, 255))
+        tick = clock.tick(10) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        group.draw(screen)
+        group.update(tick)
+        pygame.display.flip()
+    pygame.quit()
---
+++ enemy.py
@@ -0,0 +1,38 @@
+import pygame
+
+from GameObject_class import GameObject
+
+
+class Enemy(GameObject):
+    def __init__(self, group, animations, size, cords, hp):
+        super().__init__(group, animations, size, cords, (180, 200))
+
+        self.hp = hp
+
+    def update(self, time):
+        super().update(time)
+        if self.hp <= 0 and self.rect.topright[0] < 0:
+            self.kill()
+            print('I am dead')
+
+    def damage(self, damage):
+        self.hp -= damage
+
+if __name__ == '__main__':
+    pygame.init()
+    group = pygame.sprite.Group()
+    enemy1 = Enemy(group, ['Player', 'deadpool.png'], (300, 350), (600, 350), 300)
+    screen = pygame.display.set_mode((300, 500))
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        enemy1.damage(1)
+        screen.fill((255, 255, 255))
+        tick = clock.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        group.draw(screen)
+        group.update(tick)
+        pygame.display.flip()
+pygame.quit()
