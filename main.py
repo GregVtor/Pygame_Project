0199cd420f52168c3bec433dc08bbd6d8cab8829
Savva <savva2006@bk.ru>
2023-01-22 21:25:22 +0300

    starting_collect

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import pygame.sprite
+
 from modul import *
+
+
+def start_screen(clock, screen):
+    running = True
+    group = pygame.sprite.Group()
+    sprite = GameButton(group, {'start': [[]]})
+
+
+
+    def stop():
+        running = False
+
+
+    while running:
+        pass
