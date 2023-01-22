f5d7428d7f916cdeaa1b5386f28eb938aba72693
grisha <Vtorovstugreg@gmail.com>
2023-01-22 21:50:08 +0300

    debug

---
+++ GameObject_class.py
@@ -0,0 +1,73 @@
+import os
+from math import cos, sin, pi
+
+import pygame
+
+
+class GameObject(pygame.sprite.Sprite):
+    def __init__(self, group, animations, size, cords, speed_vector):
+        super().__init__(group)
+        self.frames = []
+        self.not_stop = False
+        self.frame_count = 1
+        self.cords = cords
+        self.size = size
+        self.animations = animations
+        self.animate('start', True)
+        self.image = self.frames[0]
+        self.rect = self.image.get_rect()
+        self.init_vector(speed_vector)
+
+    def image_con(self, image_adr):
+        adr = os.path.join('../data', 'texture', *image_adr)
+        return pygame.image.load(adr)
+
+    def init_vector(self, speed_vector):
+        self.speed_x, self.speed_y = speed_vector[1] * cos(speed_vector[0] * pi / 180), \
+                                     speed_vector[1] * sin(speed_vector[0] * pi / 180)
+
+    def update(self, time):
+        self.rect.move_ip(self.speed_x * time, self.speed_y * time)
+        self.image = self.frames[self.frame_count - 1]
+        if self.not_stop:
+            self.frame_count = (self.frame_count + 1) % len(self.frames)
+        elif self.frame_count == len(self.frames):
+            self.frame_count = 1
+            self.animate('start', True)
+        else:
+            self.frame_count += 1
+
+    def animate(self, key, not_stop=False):
+        if not_stop:
+            self.not_stop = True
+        else:
+            self.not_stop = False
+        self.frames = []
+        image, row, col = self.animations[key]
+        image = self.image_con(image)
+        w, h = image.get_size()[0] // col, image.get_size()[1] // row
+        for i in range(row):
+            for j in range(col):
+                sup = pygame.Surface((w, h))
+                sup.blit(image, (0, 0), (w * j, h * i, w, h))
+                self.frames.append(sup.convert())
+
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((600, 500))
+    clock = pygame.time.Clock()
+    running = True
+    group = pygame.sprite.Group()
+    sprite = GameObject(group, {'start': [['pygame-8-1.png'], 2, 8]},
+                        [50, 50], [50, 200], [0, 0])
+    while running:
+        screen.fill((255, 255, 255))
+        tick = clock.tick(10) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        group.draw(screen)
+        group.update(tick)
+        pygame.display.flip()
+    pygame.quit()
---
+++ player_class.py
@@ -0,0 +1,48 @@
+import pygame
+from modul.GameObject_class import GameObject
+
+
+class Player(GameObject):
+    def __init__(self, group, animations, size, cords,
+                 speed_vector):
+        super().__init__(group, animations, size, cords, speed_vector)
+        self.bool_jump = False
+        self.pomenal = False
+        self.high_of_jump = 200
+
+    def jump(self, tick):
+        if not self.pomenal and self.rect.bottomleft[1] <= self.high_of_jump:
+            self.pomenal = True
+            self.init_vector([90, 200])
+        elif self.pomenal and self.cords[1] <= self.rect.topleft[-1]:
+            self.bool_jump = False
+            self.pomenal = False
+            self.inint_vector([0, 0])
+
+    def update(self, time):
+        super().update(time)
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            self.bool_jump = True
+        if self.bool_jump:
+            if not self.pomenal:
+                self.init_vector([-90, 200])
+            self.jump(time)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((600, 800))
+    clock = pygame.time.Clock()
+    running = True
+    group = pygame.sprite.Group()
+    sprite = Player(group, {'statick': ['Player', 'deadpool.png']}, [300, 350], [50, 800 - 350], [0, 0])
+    while running:
+        screen.fill((255, 255, 255))
+        tick = clock.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        group.draw(screen)
+        group.update(tick)
+        pygame.display.flip()
+    pygame.quit()
