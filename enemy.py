1a0a60a3a225d5de4d173c18a42fc384823b028b
grisha <Vtorovstugreg@gmail.com>
2023-01-22 13:54:08 +0300

    debug

--- enemy.py
+++ enemy.py
@@ -18,21 +18,21 @@ class Enemy(GameObject):
     def damage(self, damage):
         self.hp -= damage
 
-
-pygame.init()
-group = pygame.sprite.Group()
-enemy1 = Enemy(group, ['Player', 'deadpool.png'], (300, 350), (600, 350), 300)
-screen = pygame.display.set_mode((300, 500))
-clock = pygame.time.Clock()
-running = True
-while running:
-    enemy1.damage(1)
-    screen.fill((255, 255, 255))
-    tick = clock.tick(60) / 1000
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    group.draw(screen)
-    group.update(tick)
-    pygame.display.flip()
+if __name__ == '__main__':
+    pygame.init()
+    group = pygame.sprite.Group()
+    enemy1 = Enemy(group, ['Player', 'deadpool.png'], (300, 350), (600, 350), 300)
+    screen = pygame.display.set_mode((300, 500))
+    clock = pygame.time.Clock()
+    running = True
+    while running:
+        enemy1.damage(1)
+        screen.fill((255, 255, 255))
+        tick = clock.tick(60) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        group.draw(screen)
+        group.update(tick)
+        pygame.display.flip()
 pygame.quit()
