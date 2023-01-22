b13c4d808ea43ca7bdf20411946d2ea4ca7a22e5
Savva <savva2006@bk.ru>
2023-01-22 21:33:19 +0300

    starting_collect_again

--- main.py
+++ main.py
@@ -5,14 +5,22 @@ from modul import *
 
 def start_screen(clock, screen):
     running = True
-    group = pygame.sprite.Group()
-    sprite = GameButton(group, {'start': [[]]})
 
-
-
-    def stop():
+    def stop(*args, **kwargs):
         running = False
 
+    group = pygame.sprite.Group()
+    sprite = GameButton(group, {'start': [['backgrounds', 'Backgrounds.png'],
+                                          1, 1]}, (600, 900), stop)
 
     while running:
-        pass
+        tick = clock.tick(60) / 1000
+        screen.fill('red')
+
+        for event in pygame.event.get():
+            if event.type == pygame.quit():
+                exit(0)
+
+        group.update(tick)
+        group.draw(screen)
+        pygame.display.flip()
