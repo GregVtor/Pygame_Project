74520e3a12d8f10d28fd0338f5f77be8a38b0d39
Savva <savva2006@bk.ru>
2023-01-23 22:09:43 +0300

    add_login

--- main.py
+++ main.py
@@ -59,8 +59,52 @@ def register(screem, Clock):
             col -= 1
 
 
-def login(screen, clock): ...  # Сава пиши тут
+def login(screen, clock): # Сава пиши тут
+    running = True
+    text = ''
+    font = pygame.font.Font(font_adr('Pixel Times.ttf'), 56)
+    col = 0
 
+    def check(*args, **kwargs):
+        nonlocal running
+        nonlocal text
+        error = False
+        if not text:
+            return
+        if db_con(f'SELECT id FROM Users WHERE Name == "{text}"').fetchone():
+            running = False
+        elif not db_con(f'SELECT id FROM Users WHERE Name == "{text}"').fetchone():
+            error = True
+            font = pygame.font.Font(font_adr('Pixel Times.ttf'), 64)
+            text1 = font.render('Hello Привет', False, (180, 0, 0))
+
+            screen.blit(text1, (10, 50))
+
+            pygame.display.update()
+
+
+
+    group = pygame.sprite.Group()
+    sprite = GameButton(group, {'start': [['login_btn.png'],
+                                          1, 1]}, (6, 900), (150, 500), check)
+
+    while running:
+        text_s = font.render(text, False, 'red')
+        screen.fill((220, 180, 0))
+        tick = Clock.tick(FPS) / 1000
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN and event.unicode.lower() in ALLOW and len(
+                    text) <= 13:
+                text += event.unicode
+        if pygame.key.get_pressed()[pygame.K_BACKSPACE] and col == 0:
+            text = text[:-1]
+            col = 10
+        screen.blit(text_s, (100, 400))
+        group.draw(screen)
+        group.update(tick)
+        pygame.display.flip()
 
 def start_screen(screen, clock):
     running = True
