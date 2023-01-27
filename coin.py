d3a4a54ff21f898b2e7dd811645dace120c87106
GregVtor <101187091+GregVtor@users.noreply.github.com>
2023-01-27 12:28:28 +0300

    Update coin.py

--- coin.py
+++ coin.py
@@ -2,5 +2,16 @@ from GameObject_class import GameObject
 
 
 class Coin(GameObject):
-    def __init__(self, group, animations, cords):
+    def __init__(self, group, animations, cords, player, func_add):
         super().__init__(group, animations, (35, 35), cords, (180, 200))
+        self.player = player
+        self.func_add = func_add
+
+    def update(self, tick):
+        super().update(tick)
+        if self.rect.colliderect(self.player):
+            self.func_add()
+            self.kill()
+        if self.rect.topright[0] <= 0:
+            self.kill()
+            
